e1fab04996a4e221ea0aac84d2f1e43af7986961
nelsonhe <nelsonghernandez@gmail.com>
2026-02-10 20:16:20 -0500

    Ejercicio 6

---
+++ Ejercicio6/Product.java
@@ -0,0 +1,13 @@
+public class Product implements Item {
+    private String name;
+    private double price;
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    @Override
+    public double getPrice(){
+        return price;
+    }
+}
